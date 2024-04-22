edddc1a166e528006ec325aa6f2aca4e6ec80f32
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-04-22 15:31:35 +0700

    display dan accept panjang dan lebar

--- oop2.cpp
+++ oop2.cpp
@@ -10,5 +10,9 @@ class bangunDatar{
 
         void input(){//methode input persegi panjang
 
+            cout << "Masukan Panjangnya : ";
+            cin >> panjang;
+            cout << "Masukan Lebarnya : ";
+            cin >> lebar;
         }
 };

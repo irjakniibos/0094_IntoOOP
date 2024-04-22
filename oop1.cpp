165ae04e6c752d32df4e6df980919559cc455d62
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-04-22 15:11:31 +0700

    membuat prosedur input

--- oop1.cpp
+++ oop1.cpp
@@ -20,6 +20,16 @@ class Matakuliah{
         string namaMk;
         int sks;
 
-};
+
 
     public:
+        void input(){
+            cout << "Kode MK : " ;
+            cin >> kodeMk ;
+            cout << "Nama MK : " ;
+            cin >> namaMk ;
+            cout << "SKS : ";
+            cin >> sks ;
+        }
+
+};
